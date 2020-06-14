759ffd94dab73b8ecc30e740f9fc7336edf64f5e
Lucas V. Araujo <lucas.vieira.ar@disroot.org>
2020-06-14 11:04:29 -0300

    first release

---
+++ iperl.pl
@@ -0,0 +1,22 @@
+use warnings;
+use Term::ReadLine;
+use Term::ANSIColor;
+
+$SIG{__WARN__} = \&handle_warnings;
+my $term = new Term::ReadLine 'IPerl';
+my $command;
+
+do
+{
+    $command = $term->readline(color('blue') . "IPerl" . color('white') . ">" . color('reset') . " ");
+    exit if $command =~ /^exit(\(\))|;$/;
+    my $output = eval($command);
+    print(color('red') . "ERROR: " . color('reset') .  $@ . "\n") if $@;
+    print($output . "\n") if $output;
+}
+while ($command !~ /^exit(\(\))|;$/);
+
+sub handle_warnings
+{
+    print color('yellow'). "WARNING: " . color('reset') . shift . "\n" 
+}
