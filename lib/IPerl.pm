32b282e543fbc22575e76545161f3c523804f231
Lucas V. Araujo <lucas.vieira.ar@disroot.org>
2021-07-26 09:38:41 -0300

    added IPerl::Term

--- lib/IPerl.pm
+++ lib/IPerl.pm
@@ -3,7 +3,7 @@ use JSON;
 use strict;
 use warnings;
 use lib './';
-use Term::ReadLine;
+use IPerl::Term;
 use IPerl::CodeExec;
 
 our $VERSION = "1.1.1";
@@ -46,33 +46,11 @@ sub new
             close $file;
         }
     }
-    my $term    = Term::ReadLine->new($prompt);
-    my $attr    = $term->Attribs;
-    $term->ornaments(0);
-    $term->read_history($history);
-    $term->using_history();
-    $attr->{completion_entry_function} = $attr->{list_completion_function};
-    $attr->{completion_word} = [
-        qw(
-            chomp chop chr crypt fc hex index lc lcfirst length oct
-            ord pack q// qq// reverse rindex sprintf substr tr/// uc
-            ucfirst y/// m// pos qr// quotemeta s/// split study print
-            abs atan2 cos exp hex int long oct rand sin sqrt srand each
-            keys pop push shift splice unshift grep join map qw// reverse 
-            sort unpack delete each exists values binmode close closedir
-            dbmclose dbmopen die eof fileno flock format getc printf read
-            readdir readline rewinddir say seek seekdir select syscall
-            sysread sysseek syswrite tell telldir truncate warn write break
-            if next return last exit continue kill fork exec system pipe
-            wait waitpid lock scalar do require import use ref package
-            accept bind connect getpeername getsockname getsockopt listen
-            recv send setsockopt shutdown socket socketpair eval
-            searchModule listModules refreshModules loadModule
-        )
-    ];
+    my $term    = IPerl::Term->new(historyfile => $history);
+    $term->history_load();
     bless {
         path => $path, prompt => $prompt, history => $history,
-        term => $term, term_attribs => $attr, config => $config,
+        term => $term, config => $config,
         default => $default,
     }, $self;
 }
@@ -80,14 +58,19 @@ sub new
 sub run
 {
     my ($self, %args) = @_;
-    #
+    #print the intro
     print $args{intro}, "\n" if $args{intro};
-    #
-    my $words = $self->{term_attribs}->{completion_word};
+    #get the IPerl::Term instance
+    my $term = $self->{term};
+    #get a reference to a completion array
+    $self->{custom_completion} = $term->completion_array([]);
+    #get the prompt
+    my $prompt = $self->{prompt};
     #support for IPerl modules
     $INC{IPERL_MODULES} = $self->{path};
+    #add the modules to the path
     unshift @INC, @{$self->{path}};
-    
+
     #Give IPerl::CodeExec access to the current running IPerl instance
     $IPerl::CodeExec::IPERL_INSTANCE = $self;
     #load default modules
@@ -95,21 +78,21 @@ sub run
     
     while (1)
     {
-        my $code = $self->{term}->readline("$self->{prompt}> ");
+        my $code = $term->readline("$prompt> ");
         next unless $code;
         #check if the input is a multi-line code (I'll need to refact this later)
         if ($code =~ /(\{|\(|\[)$/)
         {
             do
             {
-                $_ = $self->{term}->readline("... ");
+                $_ = $term->readline("... ");
                 $code .= $_ if $_
             } while ($_);
         }
         #add user-defined function names to the completion list
         $self->add_completion(map { $1 } $code =~ /sub ([\d\w_]+)/g);
         #save the history at each command
-        $self->{term}->write_history($self->{history});
+        $term->history_save();
         #evaluate the user code on a different scope to avoid conflicts with
         #identifiers of the IPerl internal code
         my ($output, $error, $warning) = IPerl::CodeExec::__evaluate($code);
@@ -123,7 +106,7 @@ sub run
 sub add_completion
 {
     my ($self, @words) = @_;
-    my $list = $self->{term_attribs}->{completion_word};
+    my $list = $self->{custom_completion};
     for my $name (@words)
     {
         my $search = quotemeta($name);
